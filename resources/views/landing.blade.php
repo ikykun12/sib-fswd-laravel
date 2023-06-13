d5a8c2cdd2c176a0a4da5b0c1d8eeabc1f1bc06e
ikykun12 <pranatarizky858@gmail.com>
2023-06-13 20:32:30 +0700

    bug contact

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -7,8 +7,6 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Ecommer - One Stop Shopping</title>
-
-
     <!-- Bootstrap icons-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
     <!-- Google Font -->
